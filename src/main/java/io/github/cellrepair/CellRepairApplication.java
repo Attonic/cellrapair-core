b99871ca7a7baa55613674c0173d2cceef784db7
Antonio <a.cleison@live.com>
2026-02-24 21:48:11 -0300

    Commit inicial

---
+++ src/main/java/io/github/cellrepair/CellRepairApplication.java
@@ -0,0 +1,13 @@
+package io.github.cellrepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellRepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellRepairApplication.class, args);
+	}
+
+}
